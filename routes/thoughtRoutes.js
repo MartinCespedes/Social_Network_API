a61fda9035b83cb22f6e46653bd1b78a9b67d207
MartinCespedes <martin.cespedes002@mymdc.net>
2023-02-10 21:14:56 -0500

    Created thoughtRoutes.js with routes for thoughts & thought id

---
+++ routes/thoughtRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const { Thought } = require('../models');
+
+// Get all thoughts //
+router.get('/', async (req, res) => {
+  try {
+    const thoughts = await Thought.find();
+    res.json(thoughts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Get a single thought by ID //
+router.get('/:id', async (req, res) => {
+  try {
+    const thought = await Thought.findById(req.params.id);
+    if (!thought) {
+      return res.status(404).json({ message: 'Thought not found' });
+    }
+    res.json(thought);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});

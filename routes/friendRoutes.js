91d54196965cc3d11361879fd4c7bc97ed494ecf
MartinCespedes <martin.cespedes002@mymdc.net>
2023-02-10 22:03:00 -0500

    Finished friendRoutes.js with delete a friend route

--- routes/friendRoutes.js
+++ routes/friendRoutes.js
@@ -27,3 +27,15 @@ router.get("/", async (req, res) => {
     return res.status(400).json({ message: error.message });
   }
 });
+
+// delete a friend //
+router.delete("/:id", async (req, res) => {
+    try {
+      await Friend.findByIdAndDelete(req.params.id);
+      return res.status(200).json({ message: "Friend deleted." });
+    } catch (error) {
+      return res.status(400).json({ message: error.message });
+    }
+  });
+  
+  module.exports = router;

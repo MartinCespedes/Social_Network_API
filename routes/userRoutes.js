234996c2a17188531277ee0c294811141f97cec2
MartinCespedes <martin.cespedes002@mymdc.net>
2023-02-10 21:44:48 -0500

    Added create new user, update user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,3 +16,35 @@ router.get('/', async (req, res) => {
 router.get('/:id', getUser, (req, res) => {
   res.json(res.user);
 });
+
+// Create a new user //
+router.post('/', async (req, res) => {
+    const user = new User({
+      username: req.body.username,
+      email: req.body.email
+    });
+  
+    try {
+      const newUser = await user.save();
+      res.status(201).json(newUser);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  });
+  
+  // Update a user //
+  router.patch('/:id', getUser, async (req, res) => {
+    if (req.body.username != null) {
+      res.user.username = req.body.username;
+    }
+    if (req.body.email != null) {
+      res.user.email = req.body.email;
+    }
+  
+    try {
+      const updatedUser = await res.user.save();
+      res.json(updatedUser);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  });

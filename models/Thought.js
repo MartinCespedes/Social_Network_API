471b1a3329bebfdbd389ed182c0fcd8762b5908a
MartinCespedes <martin.cespedes002@mymdc.net>
2023-03-08 08:09:59 -0500

    Included Reaction.js model as a subdocument schema in Thought.js model

--- models/Thought.js
+++ models/Thought.js
@@ -1,21 +1,43 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const ThoughtSchema = new mongoose.Schema({
   text: {
     type: String,
     required: true,
     trim: true,
+    maxlength: 280,
   },
   createdAt: {
     type: Date,
     default: Date.now,
   },
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
+  username: {
+    type: String,
+    required: true,
   },
+  reactions: [
+    {
+      reactionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
 });
 
-const Thought = mongoose.model('Thought', ThoughtSchema);
+const Thought = mongoose.model("Thought", ThoughtSchema);
 
 module.exports = Thought;
